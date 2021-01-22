5edc2684091e77e86603651d76cdd07735ad9f4d
ReyizDeniZ <pocketcocuk@gmail.com>
2021-01-22 16:18:19 +0300

    Delete events directory

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client) => {
-
-    console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
-
-    client.user.setActivity("v1.4 | ?help");
-
-};
